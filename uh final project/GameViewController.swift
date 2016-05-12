52973be8227197d0d6e82aaf8f403e7b5c548b0e
Emily DeJesu <student@edejesu.local>
2016-05-12 10:21:49 -0500

    new bricks

--- uh final project/GameViewController.swift
+++ uh final project/GameViewController.swift
@@ -17,44 +17,53 @@ class GameViewController: UIViewController, UICollisionBehaviorDelegate
     var gravity: UIGravityBehavior!
     var collision: UICollisionBehavior!
     var ball = UIView()
+    var brick = UIView()
+    var brickArray : [UIView] = []
+    var timer = NSTimer()
+    var counter = 0
 
     
-    @IBOutlet weak var brickView: UIView!
-
-  
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         animator = UIDynamicAnimator(referenceView: view)
 
-
-     brickView.backgroundColor = UIColor(patternImage: UIImage(named: "brick")!)
-        //example square
         ball = UIView(frame: CGRect(x: 100, y: 100, width: 20, height: 20))
         ball.backgroundColor = UIColor.grayColor()
         view.addSubview(ball)
         ball.layer.cornerRadius = 10
         ball.clipsToBounds = true
         // ball properities
-        addBehaviorsToBricks()
-
-
-        //makes ball fall
-       // animator = UIDynamicAnimator(referenceView: brickView)
-       gravity = UIGravityBehavior(items: [ball])
-       animator.addBehavior(gravity)
-        //making boundries
-     //   collision.addBoundaryWithIdentifier("barrier", forPath: UIBezierPath(rect: barrier.frame))
-        //makes barries stay in place when hit by the block
-        //stays in frame
-        movingBricks()
-        //behaviors to moving brick
+      //  addBehaviorsToBricks()
+      // gravity = UIGravityBehavior(items: [ball])
+    //   animator.addBehavior(gravity)
+       
+      //  movingBricks()
    
+    }
+    @IBAction func startButtonTapped(sender: UIButton)
+    {
+        addBehaviorsToBricks()
+        gravity = UIGravityBehavior(items: [ball])
+        animator.addBehavior(gravity)
+        movingBricks()
+        startButton.alpha = 0
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(GameViewController.timerAction), userInfo: nil, repeats: true)
+    }
+    func timerAction()
+    {
+        counter += 1
+        timerLabel.text = "\(counter)"
     }
     
     func addBehaviorsToBricks()
     {
-        let brickBehavior = UIDynamicItemBehavior(items: [brickView])
+        for brick in brickArray
+        {
+        let brickBehavior = UIDynamicItemBehavior(items: [brick])
         brickBehavior.allowsRotation = false
         brickBehavior.density = 10000
         brickBehavior.resistance = 0.0
@@ -65,9 +74,17 @@ class GameViewController: UIViewController, UICollisionBehaviorDelegate
         ballBehavior.elasticity = 0.0
         ballBehavior.density = 0.0
         animator.addBehavior(ballBehavior)
-        
-
-
+        }
+    }
+    
+    // RANDOM LINE BREAKS
+    func lineBreaks()
+    {
+        for bricks in brickArray
+        {
+            bricks.willRemoveSubview(bricks)
+            //make this random
+        }
     }
     @IBAction func Swipped(sender: UISwipeGestureRecognizer)
     {
@@ -77,25 +94,44 @@ class GameViewController: UIViewController, UICollisionBehaviorDelegate
     
    func movingBricks()
    {
+    NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(GameViewController.makeBricks), userInfo: nil, repeats: true)
+
     NSTimer.scheduledTimerWithTimeInterval(0.04, target: self, selector: #selector(GameViewController.obstacleMove), userInfo: nil, repeats: true)
-    collision = UICollisionBehavior(items: [ball, brickView])
-    collision.translatesReferenceBoundsIntoBoundary = true
+    collision = UICollisionBehavior(items: [ball])
+    collision.translatesReferenceBoundsIntoBoundary = false
     animator.addBehavior(collision)
 
-  //  collision.addBoundaryWithIdentifier("ball", forPath: UIBezierPath(rect: ball.frame))
-//    collision.addBoundaryWithIdentifier("brickView", forPath: UIBezierPath(rect: brickView.frame))
-
-
-  
-
-    }
-    func obstacleMove(){
-        brickView.center = CGPointMake(brickView.center.x, brickView.center.y-1)
-        brickView.center = CGPointMake(brickView.center.x, brickView.center.y-1)
-        animator.updateItemUsingCurrentState(brickView)
-        animator.updateItemUsingCurrentState(ball)
     }
     
+    
+    func makeBricks()
+    {
+        brickArray = []
+         let brick = UIView(frame: CGRectMake(0, 600, 500, 30))
+        brick.backgroundColor = UIColor.redColor()
+        //(patternImage: UIImage(named: "brick")!)
+        view.addSubview(brick)
+        brickArray.append(brick)
+    }
+    
+    func obstacleMove(){
+       
+        for brick in brickArray
+        {
+        
+        brick.center = CGPointMake(brick.center.x, brick.center.y-1)
+        brick.center = CGPointMake(brick.center.x, brick.center.y-1)
+        animator.updateItemUsingCurrentState(brick)
+        animator.updateItemUsingCurrentState(ball)
+        }
+    }
+    
+    //transfer of data?
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        var highScoreView = segue.destinationViewController as! HighScoreViewController
+    }
     
 
 }
---
+++ uh final project/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  uh final project
+//
+//  Created by edejesu on 5/10/16.
+//  Copyright © 2016 edejesu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "newPizza")!)
+        
+    }
+
+  
+}
